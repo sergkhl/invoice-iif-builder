774582740bd14d94a9ba88a428a887e48e56873c
sergio <sergio@priveapp.com>
2018-11-12 06:28:08 -0800

    remove tax fields write account list

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Utility library for parsing CSV files for use with QuickBooks.
 Input format <DOMAIN_INFO|DESCRIPTION|QUANTITY|UNIT_CHARGE>
 
 
-Output will be written to ./output.iif
-
+Output will be written to ./transactions.iif  
+Account list will be written to ./accnt-list.iif
 
 Usage:
 node ./index.js <filename.csv>
--- index.js
+++ index.js
@@ -4,7 +4,8 @@
  * This is CSV to QuickBooks IIF converter
  * Input format <DOMAIN_INFO|DESCRIPTION|QUANTITY|UNIT_CHARGE>
  *
- * Output will be written to ./output.iif
+ * Output will be written to ./transactions.iif
+ * Account list will be written to ./accnt-list.iif
  * Usage:
  *
  *     node ./index.js <filename.csv>
@@ -14,7 +15,8 @@ var path = process.argv[1]
 var file = process.argv[2]
 var fs = require('fs')
 var source = process.cwd() + '/' + file
-var destination = process.cwd() + '/output.iif'
+var destination = process.cwd() + '/transactions.iif'
+var accntListDestination = process.cwd() + '/accnt-list.iif'
 if (!file) {
     return console.log("You're going to need to specify a .csv file to process, or this will go nowhere.")
 }
@@ -43,12 +45,14 @@ function main() {
     var currentCustomer
     var invoiceDate
     var currentInvoice
+    var transactionMEMO
     for (var i = 0; i < lines.length; i++) {
         columns = lines[i].split('|')
         if (columns.length === 4) {
             currentCustomer = columns[0].slice(8) // Slice 'DOMAIN: ' part
             if (currentCustomer !== lastCustomer) { // Get invoice date from the first row description
-                var usageEndDate = columns[1].split(' - ')[1]
+                transactionMEMO = columns[1]
+                var usageEndDate = transactionMEMO.split(' - ')[1]
                 var usageEndDateArray = usageEndDate.split('/')
                 var monthNum = months.indexOf(usageEndDateArray[0]) + 1
                 invoiceDate = monthNum + '/' + usageEndDateArray[1] + '/' + usageEndDateArray[2]
@@ -57,27 +61,35 @@ function main() {
                 invoices.push(currentInvoice)
                 continue
             }
-            var price = roundTo(columns[3], 2)
-            currentInvoice.push({
-                TRNSTYPE: 'INVOICE',
-                DATE: invoiceDate,
-                ACCNT: currentCustomer + ' Accounts Receivable',
-                secondaryACCNT: currentCustomer + ' Income Account',
-                NAME: currentCustomer,
-                CLASS: '',
-                AMOUNT: roundTo(columns[2] * price, 2),
-                MEMO: columns[1],
-                QNTY: columns[2],
-                PRICE: columns[3],
-                INVITEM: columns[1],
-                TAXABLE: 'N',
-                REIMBEXP: 'NOTHING'
-            })
+            var price = roundTo(columns[3], 5)
+            var amount = roundTo(columns[2] * price, 5)
+            if (amount !== 0) { // exclude items with 0 price
+                currentInvoice.push({
+                    TRNSTYPE: 'INVOICE',
+                    DATE: invoiceDate,
+                    ACCNTTYPE: 'AR',
+                    secondaryACCNTTYPE: 'INC',
+                    ACCNT: 'Accounts Receivable ' + currentCustomer,
+                    secondaryACCNT: 'Income Account',
+                    NAME: currentCustomer,
+                    CLASS: '',
+                    AMOUNT: amount,
+                    transactionMEMO: transactionMEMO,
+                    MEMO: columns[1],
+                    QNTY: columns[2],
+                    PRICE: price,
+                    INVITEM: columns[1],
+                    TAXABLE: 'N',
+                    REIMBEXP: 'NOTHING'
+                })
+            }
         }
     }
 
-    fs.writeFileSync(destination, '!TRNS\tTRNSTYPE\tDATE\tACCNT\tNAME\tCLASS\tAMOUNT\tMEMO\n!SPL\tTRNSTYPE\tDATE\tACCNT\tNAME\tCLASS\tAMOUNT\tMEMO\tQNTY\tPRICE\tINVITEM\tTAXABLE\tREIMBEXP\n!ENDTRNS\n')
+    fs.writeFileSync(destination, '!TRNS\tTRNSTYPE\tDATE\tACCNT\tNAME\tCLASS\tAMOUNT\tMEMO\n!SPL\tTRNSTYPE\tDATE\tACCNT\tNAME\tCLASS\tAMOUNT\tMEMO\tQNTY\tPRICE\n!ENDTRNS\n')
     format(invoices)
+    fs.writeFileSync(accntListDestination, '!ACCNT\tNAME\tACCNTTYPE\n')
+    formatAccntList(invoices)
 }
 
 // Write to file single transaction for each DOMAIN with multiple distribution lines
@@ -88,19 +100,32 @@ function format(invoices) {
     for (var i = 0; i < invoices.length; i++) {
         invoice = invoices[i]
         invoiceTotal = invoice.reduce(function (total, transaction) {
-            return total + transaction.AMOUNT
+            return roundTo(total + transaction.AMOUNT, 5)
         }, 0)
         text = ""
         text += 'TRNS\t"' + invoice[0].TRNSTYPE + '"\t"' + invoice[0].DATE + '"\t"' + invoice[0].ACCNT + '"\t"' + invoice[0].NAME + '"\t"'
-            + invoice[0].CLASS + '"\t"' + invoiceTotal + '"\t"' + invoice[0].MEMO + '"\n'
+            + invoice[0].CLASS + '"\t"' + invoiceTotal + '"\t"' + invoice[0].transactionMEMO + '"\n'
         for (var j = 0; j < invoice.length; j++) {
             var transaction = invoice[j]
             text += 'SPL\t"' + transaction.TRNSTYPE + '"\t"' + transaction.DATE + '"\t"' + transaction.secondaryACCNT + '"\t"' + transaction.NAME + '"\t"'
-                + transaction.CLASS + '"\t"' + -1 * transaction.AMOUNT + '"\t"' + transaction.MEMO + '"\t"' + -1 * transaction.QNTY + '"\t"' + transaction.PRICE + '"\t"'
-                + transaction.INVITEM + '"\t"' + transaction.TAXABLE + '"\t"' + transaction.REIMBEXP + '"\n'
+                + transaction.CLASS + '"\t"' + -1 * transaction.AMOUNT + '"\t"' + transaction.MEMO + '"\t"' + -1 * transaction.QNTY + '"\t"' + transaction.PRICE + '"\n'
+                /* + '"\t"' + transaction.INVITEM + '"\t"' + transaction.TAXABLE + '"\t"' + transaction.REIMBEXP*/
         }
         text += "ENDTRNS\n"
         fs.appendFileSync(destination, text)
+    }
+}
+
+function formatAccntList(invoices) {
+    var invoice
+    var text
+    text = 'ACCNT\t"' + invoices[0][0].secondaryACCNT + '"\t"' + invoices[0][0].secondaryACCNTTYPE + '"\n'
+    fs.appendFileSync(accntListDestination, text)
+    for (var i = 0; i < invoices.length; i++) {
+        invoice = invoices[i]
+        text = ""
+        text += 'ACCNT\t"' + invoice[0].ACCNT + '"\t"' + invoice[0].ACCNTTYPE + '"\n'
+        fs.appendFileSync(accntListDestination, text)
     }
 }
 
